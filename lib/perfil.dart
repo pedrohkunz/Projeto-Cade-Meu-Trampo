f92e624d636a2b3a78e32c048d2ba5f218e220e4
Pedro Kunz <pedrokunz094@gmail.com>
2023-03-16 21:56:30 -0300

    adicionando singlechieldscrollview

--- lib/perfil.dart
+++ lib/perfil.dart
@@ -20,141 +20,143 @@ class _MyProfileState extends State<MyProfile> {
       backgroundColor: Color.fromARGB(255, 22,24,30),
       drawer: MyDrawer(),
       appBar: PreferredSize(child: MyNavBar(), preferredSize: Size(55, 55)),
-      body: Container(
-        child: Center(
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  color: Color.fromARGB(255,16,17,21),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top:20.0, bottom: 10),
-                        child: Text("Seu perfil", style: TextStyle(
-                          color: Colors.white, fontSize: 35,
-                        ),),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 90, right: 90),
-                        child: Divider(
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                      ),
-                      
-                             Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage("jackie.png"),
-                          radius: 80,
-                        ),
-                      ),
-                             Padding(
-                         padding: const EdgeInsets.only(bottom:1.0),
-                         child: Text("Jackie Chan", style: TextStyle(
-                          color: Colors.white, fontSize: 26
-                         ),),
-                       ),
-                          
-                       SizedBox(height: 1.5,),
-                          
-                       Text("Ator", style: TextStyle(
-                        color: Color.fromARGB(255, 172, 171, 171), fontSize: 20
-                       ),
-                       ),
-
-                       Padding(
-                         padding: const EdgeInsets.only(top: 25),
-                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                shape: MaterialStatePropertyAll(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(40) )
-                                ),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 23, 51, 134)),
-                                padding: MaterialStateProperty.all(EdgeInsets.only(top:20, bottom: 20 ,left: 40, right: 40 ),
-                                
-                                ),
-                            ),
-                            onPressed: (){}, 
-                            child: Text("Seguir", style: TextStyle(color: Colors.white, fontSize: 18),) 
-                            ),
-                       
-                            SizedBox(width: 30),
-                       
-                            ElevatedButton(
-                              style: ButtonStyle(
-                                shape: MaterialStatePropertyAll(
-                                  RoundedRectangleBorder(
-                                    side: BorderSide(color: Color.fromARGB(255, 23, 51, 134), width: 3),
-                                    borderRadius: BorderRadius.circular(40) )
-                                ),
-                                backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 16, 17, 21)),
-                                padding: MaterialStateProperty.all(EdgeInsets.only(top:20, bottom: 20 ,left: 40, right: 40 ),
-                                
-                                ),
-                            ),
-                            onPressed: (){}, 
-                            child: Text("Contatar", style: TextStyle(color: Colors.white, fontSize: 18),) 
-                            ),
-                            
-                          ],
-                         ),
-                       ),
-
-                       SizedBox(
-                        height: 25,
-                       ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Center(
+            child: Container(
+              child: Column(
+                children: [
+                  Container(
+                    color: Color.fromARGB(255,16,17,21),
                     child: Column(
                       children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top:20.0, bottom: 10),
+                          child: Text("Seu perfil", style: TextStyle(
+                            color: Colors.white, fontSize: 35,
+                          ),),
+                        ),
                         Container(
-                          alignment: AlignmentDirectional.topStart,
-                          child: const Text("Sobre", style: TextStyle(color: Colors.white, fontSize: 25), )
+                          margin: EdgeInsets.only(left: 90, right: 90),
+                          child: Divider(
+                            color: Color.fromARGB(255, 255, 255, 255),
                           ),
-                        SizedBox(height: 13,),
-                        Text("Chan Kong-sang Embaixador da boa vontade da UNICEF, mais conhecido como Jackie Chan, é um ator, produtor, roteirista, coreógrafo, diretor de cinema, cantor e especialista honconguês em artes marciais,", style: TextStyle(color: Colors.white, fontSize: 15),),
-                        SizedBox(height: 50,),
-                        Column(
-                          children: [
-                            Text("Social Links", style: TextStyle(color: Colors.white, fontSize: 25),
-                            ),
-                            SizedBox(height: 20,),
-                             Padding(
-                            padding: const EdgeInsets.only(top: 15, left: 35, right: 35),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Image(image: AssetImage("facebook.png"),
-                                    height: 50, 
-                                    width: 50
+                        ),
+                        
+                               Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("jackie.png"),
+                            radius: 80,
+                          ),
+                        ),
+                               Padding(
+                           padding: const EdgeInsets.only(bottom:1.0),
+                           child: Text("Jackie Chan", style: TextStyle(
+                            color: Colors.white, fontSize: 26
+                           ),),
+                         ),
+                            
+                         SizedBox(height: 1.5,),
+                            
+                         Text("Ator", style: TextStyle(
+                          color: Color.fromARGB(255, 172, 171, 171), fontSize: 20
+                         ),
+                         ),
+      
+                         Padding(
+                           padding: const EdgeInsets.only(top: 25),
+                           child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(40) )
                                   ),
-                                Image(image: AssetImage("instagram.png"),
-                                    height: 50, 
-                                    width: 50
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 23, 51, 134)),
+                                  padding: MaterialStateProperty.all(EdgeInsets.only(top:20, bottom: 20 ,left: 40, right: 40 ),
+                                  
                                   ),
-                                Image(image: AssetImage("linkedin.png"),
-                                    height: 50, 
-                                    width: 50
+                              ),
+                              onPressed: (){}, 
+                              child: Text("Seguir", style: TextStyle(color: Colors.white, fontSize: 18),) 
+                              ),
+                         
+                              SizedBox(width: 30),
+                         
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStatePropertyAll(
+                                    RoundedRectangleBorder(
+                                      side: BorderSide(color: Color.fromARGB(255, 23, 51, 134), width: 3),
+                                      borderRadius: BorderRadius.circular(40) )
                                   ),
-                              ],
-                            ),
-                            ),
-                          ],
-                        )
+                                  backgroundColor: MaterialStatePropertyAll<Color>(Color.fromARGB(255, 16, 17, 21)),
+                                  padding: MaterialStateProperty.all(EdgeInsets.only(top:20, bottom: 20 ,left: 40, right: 40 ),
+                                  
+                                  ),
+                              ),
+                              onPressed: (){}, 
+                              child: Text("Contatar", style: TextStyle(color: Colors.white, fontSize: 18),) 
+                              ),
+                              
+                            ],
+                           ),
+                         ),
+      
+                         SizedBox(
+                          height: 25,
+                         ),
                       ],
                     ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            alignment: AlignmentDirectional.topStart,
+                            child: const Text("Sobre", style: TextStyle(color: Colors.white, fontSize: 25), )
+                            ),
+                          SizedBox(height: 13,),
+                          Text("Chan Kong-sang Embaixador da boa vontade da UNICEF, mais conhecido como Jackie Chan, é um ator, produtor, roteirista, coreógrafo, diretor de cinema, cantor e especialista honconguês em artes marciais,", style: TextStyle(color: Colors.white, fontSize: 15),),
+                          SizedBox(height: 50,),
+                          Column(
+                            children: [
+                              Text("Social Links", style: TextStyle(color: Colors.white, fontSize: 25),
+                              ),
+                              SizedBox(height: 20,),
+                               Padding(
+                              padding: const EdgeInsets.only(top: 15, left: 35, right: 35),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Image(image: AssetImage("facebook.png"),
+                                      height: 50, 
+                                      width: 50
+                                    ),
+                                  Image(image: AssetImage("instagram.png"),
+                                      height: 50, 
+                                      width: 50
+                                    ),
+                                  Image(image: AssetImage("linkedin.png"),
+                                      height: 50, 
+                                      width: 50
+                                    ),
+                                ],
+                              ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
